25ec95a6e08f4dea58c2f3873bdd1b36ee1f5674
Ubuntu <ubuntu@ip-172-31-95-44.ec2.internal>
2020-05-19 03:33:27 +0000

    new ami

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,9 +1,9 @@
 resource "aws_instance" "exp_test_ec2" {
-  ami           = "ami-0cbbbbadd7ab17426" 
+  ami           = "ami-0348bbf848e6e7430" 
   instance_type = "t2.micro"
 
-  #subnet_id = "${aws_subnet.exp_test_subnet_pub.id}"
-  subnet_id = "${aws_subnet.exp_test_subnet_priv.id}"
+  subnet_id = "${aws_subnet.exp_test_subnet_pub.id}"
+  #subnet_id = "${aws_subnet.exp_test_subnet_priv.id}"
   security_groups = ["${aws_security_group.webservers.id}"]
   key_name = "exp_key"
 
